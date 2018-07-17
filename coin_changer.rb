ed066b4cd443154860aa28de36da714670a97b3d
Jordan Endicott <jordan.endicott823@gmail.com>
2018-07-17 15:08:05 -0400

    reworked to allow massive numbers

--- coin_changer.rb
+++ coin_changer.rb
@@ -3,19 +3,25 @@ def coins(number)
 	q_count = 0; d_count = 0; n_count = 0; p_count = 0
 	ammounts = {quarter: 25, dime: 10, nickel: 5, penny: 1}
 	until number == 0
-		if number >= ammounts[:quarter]
-			number -= ammounts[:quarter]
-			q_count += 1
+		if number >= 24
+			q_count = number / 25
+			number = number - (q_count * 25)
 			coin.store(:quarter, q_count)
-		elsif number >= ammounts[:dime]
-			number -= ammounts[:dime]
-			d_count += 1
+		end
+		
+		if number >= 9
+			d_count = number / 10
+			number = number - (d_count * 10)
 			coin.store(:dime, d_count)
-		elsif number >= ammounts[:nickel]
-			number -= ammounts[:nickel]
-			n_count += 1
+		end
+		
+		if number >= 4
+			n_count = number / 5
+			number = number - (n_count * 5)
 			coin.store(:nickel, n_count)
-		elsif number >= ammounts[:penny]
+		end
+		
+		if number >= ammounts[:penny]
 			number-= ammounts[:penny]
 			p_count += 1
 			coin.store(:penny, p_count)
